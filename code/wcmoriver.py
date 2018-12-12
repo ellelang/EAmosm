a75e62b43933717df1c01d3962ce6ea56778b09b
Elle Lang <langzx@uw.edu>
2018-12-12 07:51:07 -0800

    set index for doing for loop

--- code/wcmoriver.py
+++ code/wcmoriver.py
@@ -7,6 +7,7 @@ mdata_folder = Path("C:/Users/langzx/Documents")
 
 wcmo = pd.read_excel(mdata_folder/"WCMO.xlsx")
 flowdata = pd.read_excel(mdata_folder/"SWAT_WY.xlsx")
+print(flowdata['Subbasin'])
 SBIO = pd.read_excel(mdata_folder/"SB IO Detail.xlsx")
 SBoutput = pd.read_excel(mdata_folder/"Output Subbasin Daily.xlsx")
 
@@ -18,9 +19,10 @@ SBoutput = pd.read_excel(mdata_folder/"Output Subbasin Daily.xlsx")
  Section 4: Hockey stick application
 
 '''
-
+flowdata.loc[flowdata['Subbasin'] == 1]['Water Yield'][0:3]
 WCMO_N = wcmo.shape[0]
 wcmo['select'] = np.random.randint(2, size=WCMO_N)
+
 
 # Section 1: Allocate WCMO to each HYDSB
 def allocate (mo, sb_n, mo_n):
@@ -31,7 +33,7 @@ def allocate (mo, sb_n, mo_n):
           if mo['HYDSB_LES30SB'][j] == i and mo['select'][j] == 1:
              SB_mocount [i] =  SB_mocount [i] + 1
              SB_moaream2 [i] = SB_moaream2 [i] + mo['area_m2'][j]
-  return SB_mocount,SB_aream2
+  return SB_mocount,SB_moaream2
 
 WCMOselect =  allocate (wcmo,30,WCMO_N)
 
@@ -53,14 +55,22 @@ n = 30 * 9131
 n
 SB_mocount = WCMOselect [0]
 SB_moaream2 = WCMOselect [1]
-
+len(SB_mocount )
 ### If no WCMO is selected:
-Qnout = np.zeros (n)
+SB_mocount_zero =  30 - (np.count_nonzero(SB_mocount))
+SB_mocount_zero
+Qnout = np.empty (SB_mocount_zero)
+n_zero = 0
 for i in range (0, 30):
   if SB_mocount[i] == 0:
+      n_zero = n_zero + 1
       for j in range (0, 9131):
-          Qnout[j] = flowdata.loc[flowdata['Subbasin'] == i]['Water Yeild'][j]
-      
+          index = j + (n_zero - 1) * 9131
+          #Qnout[index] = flowdata.loc[flowdata['Subbasin'] == i]['Water Yield'][j]
+          print (index)
+
+aa  = 6 * 9131
+aa
 ### When there are WCMO selected
 Aw = np.empty(30)
 Awbar = np.empty (30)
@@ -70,9 +80,13 @@ St = np.empty (n)
 Ht = np.empty (n)
 Qt = np.empty (n)
 Qtnout = np.empty (n)
+Awbar [1] / SBIO['SBarea_m2'][1]
+flowdata = flowdata.loc[flowdata["Subbasin"] == 1]
+a = flowdata['Water Yield']
+len(a)
 for i in range (0,30):
-    if WCMO_DAfactor * SB_moaream2 [i] > SBIO['SBarea_m2'] - Anmin:
-        Aw [i] = SBIO['SBarea_m2'] - Anmin
+    if WCMO_DAfactor * SB_moaream2 [i] > SBIO['SBarea_m2'][i] - Anmin [i]:
+        Aw [i] = SBIO['SBarea_m2'][i] - Anmin [i]
     else:
         Aw [i] = WCMO_DAfactor * SB_moaream2 [i]
     Awbar [i] = Aw [i] / SB_mocount [i]
@@ -82,24 +96,24 @@ for i in range (0,30):
     
     ##Set initial condition of water
     flowdata = flowdata.loc[flowdata['Subbasin'] == i]
-    It [1] = flowdata['Water Yeild'][i] * (Awbar [i] / SBIO['SBarea_m2'][i])
-    St [1] = 0
-    Ht [1] = St [1] / WCMO_SAbar [i] 
-    Qt [1] = 0
+    It [0:9130] = flowdata['Water Yield'][0:9130] * (Awbar [i] / SBIO['SBarea_m2'][i])
+    St [0:9130] = 0
+    Ht [0:9130] = St [0:9130] / WCMO_SAbar [i] 
+    Qt [0:9130] = 0
     ### iteration begins:
     for j in range (1,9131):
-        It [j] = flowdata['Water Yeild'][j] * (Awbar [i] / SBIO['SBarea_m2'][i])
+        It [9130 + j] = flowdata['Water Yield'][j] * (Awbar [i] / SBIO['SBarea_m2'][i])
         #St [j] = St [j - 1] + (It [j - 1] - Qt [j - 1] - WCMO_SAbar [i] ) * (1 * k + ET)) * dt 
-        St [j] = 1
-        Ht [j] = St [j] / WCMO_SAbar [i] 
-        if Ht [j] > WCMO_D:
-            Qt [j] = C * L * (Ht [j] - WCMO_D) ^ 1.5
+        St [9130 + j] = 1
+        Ht [9130 + j] = St [9130 + j] / WCMO_SAbar [i] 
+        if Ht [9130 + j] > WCMO_D:
+            Qt [9130 + j] = C * L * (Ht [9131 + 0] - WCMO_D) ^ 1.5
         else:
-            Qt [j] = 0
+            Qt [9131 + 0] = 0
     for j in range (0,9131):
         Itn [j] = It [j] * SB_mocount [i] 
         Stn [j] = St [j] * SB_mocount [i] 
         Htn [j] = Ht [j] * SB_mocount [i] 
         Qtn [j] = Qt [j] * SB_mocount [i]
-        Qtnout [j] = Qtn [j] + (flowdata['Water Yeild'][j] - Itn [j] )
-        flowdata['Water Yeild'][j] = Qtnout [j]
+        Qtnout [j] = Qtn [j] + (flowdata['Water Yield'][j] - Itn [j] )
+        flowdata['Water Yield'][j] = Qtnout [j]
