3f3a1d0d5d60cbff51cbc8f786bd34dec2fec8d3
Elle Lang <langzx@uw.edu>
2020-10-20 14:29:32 -0700

    spatial subplot

---
+++ code/python/MRB2020bcrRobust.py
@@ -0,0 +1,9 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import geopandas as gpd
+from geopandas import GeoSeries, GeoDataFrame
+from pathlib import Path
+import itertools
+data_folder = Path('C:/Users/langzx/Desktop/github/EAmosm/data')
